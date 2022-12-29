5135887188aad5ef86411e3e1d3b18c2548d606d
ClarkeeCity <69399383+ClarkeeCity@users.noreply.github.com>
2022-12-29 00:24:12 -0500

    fix: camera component rendering across app no more

--- src/screens/snap/SnapScreen.tsx
+++ src/screens/snap/SnapScreen.tsx
@@ -1,21 +1,41 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Text, View, Dimensions } from "react-native";
 import { useIsFocused, useTheme } from "@react-navigation/native";
 import { Camera, CameraDevice } from "react-native-vision-camera";
-
+/* import { 
+  Reanimated, 
+  Extrapolate, 
+  interpolate, 
+  useAnimatedGestureHandler, 
+  useAnimatedProps, 
+  useSharedValue 
+} from 'react-native-reanimated';
+*/
 /**
  * ? Local Imports
  */
 import createStyles from "./SnapScreen.style";
 // import Text from "@shared-components/text-wrapper/TextWrapper";
-import { Text, View } from "react-native";
 
 interface SnapScreenProps {}
 
+const windowHeight = Dimensions.get("window").height;
+console.log(windowHeight);
+
+// const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
+// Reanimated.addWhitelistedNativeProps({zoom: true});
+
+// const SCALE_FULL_ZOOM = 3;
+// const BUTTON_SIZE = 40;
+
 const SnapScreen: React.FC<SnapScreenProps> = () => {
+  // Theme
   const theme = useTheme();
   const { colors } = theme;
   const styles = useMemo(() => createStyles(theme), [theme]);
 
+  // Camera
+  const camera = useRef<Camera>(null);
   const [devices, setDevices] = useState<CameraDevice[]>();
   const isFocused = useIsFocused();
   useEffect(() => {
@@ -28,16 +48,26 @@ const SnapScreen: React.FC<SnapScreenProps> = () => {
   }, [isFocused]);
 
   if (devices == undefined) {
-    console.log("Device not found on device.");
     return (
-      <View>
-        <Text style={{ color: colors.primary100 }}>Not found</Text>
+      <View style={styles.container}>
+        <Text style={{ color: colors.primary100 }}>
+          No camera devices found. :(
+        </Text>
       </View>
     );
   }
 
   return (
-    <Camera style={styles.container} device={devices[0]} isActive={isFocused} />
+    <View style={styles.container}>
+      <Camera
+        style={{ width: "100%", height: windowHeight }}
+        ref={camera}
+        device={devices[0]}
+        preset={"photo"}
+        isActive={isFocused}
+        photo={true}
+      />
+    </View>
   );
 };
 
