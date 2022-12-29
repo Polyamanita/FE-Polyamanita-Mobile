62d4c612475146f94ef25a04be5e91ffad93323a
ClarkeeCity <69399383+ClarkeeCity@users.noreply.github.com>
2022-12-28 20:20:54 -0500

    feat: camera component added to snapscreen

--- src/screens/snap/SnapScreen.tsx
+++ src/screens/snap/SnapScreen.tsx
@@ -1,11 +1,13 @@
-import React, { useMemo } from "react";
-import { View } from "react-native";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTheme } from "@react-navigation/native";
+import { Camera, CameraDevice } from "react-native-vision-camera";
+
 /**
  * ? Local Imports
  */
 import createStyles from "./SnapScreen.style";
-import Text from "@shared-components/text-wrapper/TextWrapper";
+// import Text from "@shared-components/text-wrapper/TextWrapper";
+import { Text, View } from "react-native";
 
 interface SnapScreenProps {}
 
@@ -14,12 +16,30 @@ const SnapScreen: React.FC<SnapScreenProps> = () => {
   const { colors } = theme;
   const styles = useMemo(() => createStyles(theme), [theme]);
 
+  const [devices, setDevices] = useState<CameraDevice[]>();
+
+  useEffect(() => {
+    async function getDevices() {
+      const device = await Camera.getAvailableCameraDevices();
+      setDevices(device);
+    }
+
+    if (!devices) getDevices();
+  });
+
+  if (devices == undefined) {
+    console.log("Device not found on device.");
+    return (
+      <View>
+        <Text style={{ color: colors.primary100 }}>Not found</Text>
+      </View>
+    );
+  }
+
+  // console.log(devices[0]);
+
   return (
-    <View style={styles.container}>
-      <Text h1 color={colors.text}>
-        Snap
-      </Text>
-    </View>
+    <Camera style={styles.container} device={devices[0]} isActive={false} />
   );
 };
 
