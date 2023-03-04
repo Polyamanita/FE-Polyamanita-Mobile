91b61f9cd4be9941c249fbb001e36f32537dd15d
ClarkeeCity <69399383+ClarkeeCity@users.noreply.github.com>
2023-03-04 01:20:00 -0500

    fix: deleted unness utility file

--- src/screens/initial-screens/register/utils.ts
+++
@@ -1,7 +0,0 @@
-import { doRegister } from "api/requests";
-
-export const sendEmailConfirmation = (email: string) => {
-  doRegister(email).then((response) => {
-    console.log("send email confirm: ", response);
-  });
-};
