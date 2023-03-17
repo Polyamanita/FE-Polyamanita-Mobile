45ee69a356023eb61e6ad11a47311e3ba96bb67a
layyne <schnubnugget@gmail.com>
2023-03-17 08:23:02 -0400

    feat: journal cached, refetch on new capture(s)

--- src/screens/journal/utils.ts
+++ src/screens/journal/utils.ts
@@ -1,23 +1,42 @@
-import { Captures } from "api/constants/journal";
+import { Captures, JournalCache } from "api/constants/journal";
 import { doGetCaptures } from "api/requests";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { storeCaptures } from "redux/actions/journal-actions";
 import { ReduxStore } from "redux/store";
 
 export const useGetCaptures = () => {
   const [captures, setCaptures] = useState<Captures>();
   const [loading, setLoading] = useState<boolean>(true);
-  const userID = useSelector((store: ReduxStore) => store.userData.userID);
+
+  const userID: string = useSelector(
+    (store: ReduxStore) => store.userData.userID,
+  );
+  const { captures: cachedCaptures, refetch }: JournalCache = useSelector(
+    (store: ReduxStore) => store.journalData,
+  );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setLoading(true);
+
+    // Load from Redux cache, if available
+    if (!refetch && cachedCaptures) {
+      setCaptures(cachedCaptures);
+      setLoading(false);
+      return;
+    }
+
+    console.log("making getCaptures call");
     doGetCaptures(userID).then((result) => {
       if (result.status === 200) {
         setCaptures(result.data.captures);
+        dispatch(storeCaptures(result.data.captures));
         setLoading(false);
       }
     });
-  }, [userID]);
+  }, [cachedCaptures, refetch, userID, dispatch]);
 
   return { loading, captures };
 };
