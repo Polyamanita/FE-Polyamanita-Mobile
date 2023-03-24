952649efcb44cd61fe6e5ce6424364f5618194ca
layyne <schnubnugget@gmail.com>
2023-03-23 21:13:03 -0400

    feat: image upload and post capture working

--- src/screens/snap/utils/capture.ts
+++ src/screens/snap/utils/capture.ts
@@ -1,10 +1,18 @@
 // FILE PURPOSE:
 // Set of functions that happens when user is capturing a mushroom.
+import { CaptureInstance } from "api/constants/journal";
 import { Location } from "api/constants/location";
 import { doGetLocationFromLatlng } from "api/gmaps-requests";
-import { doGetUploadLinkAndS3Key } from "api/requests";
+import {
+  doGetCapture,
+  doGetUploadLinkAndS3Key,
+  doPostCaptures,
+  doUploadToS3,
+} from "api/requests";
 import { AxiosResponse } from "axios";
 import Geolocation from "react-native-geolocation-service";
+import { Dispatch } from "redux";
+import { queueRefetch } from "redux/actions/journal-actions";
 
 // Get the current position of the user.
 // First by getting coords of user from Geolocator library,
@@ -40,37 +48,54 @@ export const getCurrentPosition = () =>
     );
   });
 
-export const useGetLocation = (latitude: number, longitude: number) => {
-  // const [location, setLocation] = useState("hiii");
-  // const latlng = { latitude, longitude };
+// Use Redux's user ID and modelData's shroom ID to make a capture ID
+export const buildCaptureIDFromShroomalysis = (modelData: unknown) => {
+  modelData;
+  return "some-other-id";
+};
 
-  // doGetLocationFromLatlng(latlng).then((response) => {
-  //   console.log(response);
-  //   if (response.status === 200) {
-  //     // setLocation to something
-  //     // CHANGE THIS BEFORE USING !!!!!!!!!!!!!
-  //     setLocation("lol");
-  //   }
-  // });
-
-  // return location;
-  new Promise((resolve, reject) => {
-    const latlng = { latitude, longitude };
-    doGetLocationFromLatlng(latlng).then((response: AxiosResponse) => {
-      console.log(response);
-      if (response.status === 200) {
-        // Perform it here.
-        console.log(response);
-        resolve(response);
+export const getCaptureData = (
+  userID: string,
+  captureID: string,
+): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    doGetCapture(userID, captureID).then((result) => {
+      if (result.status === 200) {
+        resolve(result.data.capture);
       } else {
-        reject();
+        reject(result.data);
       }
     });
   });
 };
 
+export const stripParamsFromLink = (url: string) => {
+  return url.split("?")[0];
+};
+
+export const handlePostCapture = (
+  userID: string,
+  photoPath: string,
+  capture: CaptureInstance,
+  uploadLink: string,
+  dispatch: Dispatch,
+) => {
+  const imageUri = "file://" + photoPath;
+
+  doUploadToS3(imageUri, uploadLink).then((s3Response) => {
+    // console.log("s3 response status", s3Response.status);
+    if (s3Response.status === 200) {
+      // Post new capture to API
+      doPostCaptures(userID, [capture]).then((captureResponse) => {
+        console.log(captureResponse.data);
+        dispatch(queueRefetch());
+      });
+    }
+  });
+};
+
 // Make request to get S3 key for image upload.
-export const fetchS3Key = (userID: string) =>
+export const getS3Response = (userID: string) =>
   new Promise((resolve, reject) => {
     doGetUploadLinkAndS3Key(userID).then((response: AxiosResponse) => {
       if (response.status === 200) resolve(response.data);
